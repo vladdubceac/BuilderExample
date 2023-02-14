0589cc180f3941697b619f2fc808c4b2f1e7cd55
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-14 23:22:33 +0200

    Added sources

---
+++ src/learning/vladdubceac/builder/BuilderInterface.java
@@ -0,0 +1,8 @@
+package learning.vladdubceac.builder;
+
+public interface BuilderInterface {
+    void buildBody();
+    void insertWheels();
+    void addHeadlights();
+    Product getVehicle();
+}
---
+++ src/learning/vladdubceac/builder/implementation/Car.java
@@ -0,0 +1,28 @@
+package learning.vladdubceac.builder.implementation;
+
+import learning.vladdubceac.builder.BuilderInterface;
+import learning.vladdubceac.builder.Product;
+
+public class Car implements BuilderInterface {
+    private Product product = new Product();
+
+    @Override
+    public void buildBody() {
+        product.add("This is a body of a car");
+    }
+
+    @Override
+    public void insertWheels() {
+        product.add("4 wheels are added");
+    }
+
+    @Override
+    public void addHeadlights() {
+        product.add("2 headlights are added");
+    }
+
+    @Override
+    public Product getVehicle() {
+        return product;
+    }
+}
